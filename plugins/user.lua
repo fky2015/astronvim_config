de01ee075eb14248b2316dddd2f3b969741ff853
Feng Kaiyu <loveress01@outlook.com>
2024-02-01 20:52:14 +0800

    feat: add telescope-live-grep-args.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -78,6 +78,44 @@ return {
     end,
   },
   {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = "^1.0.0",
+        config = function()
+          require("telescope").load_extension "live_grep_args"
+          vim.print "asdfasfasdf"
+
+          local telescope = require "telescope"
+          local lga_actions = require "telescope-live-grep-args.actions"
+
+          telescope.setup {
+            extensions = {
+              live_grep_args = {
+                auto_quoting = true, -- enable/disable auto-quoting
+                -- define mappings, e.g.
+                -- ... also accepts theme settings, for example:
+                -- theme = "dropdown", -- use dropdown theme
+                -- theme = { }, -- use own theme spec
+                -- layout_config = { mirror=true }, -- mirror preview pane
+              },
+            },
+          }
+        end,
+      },
+    },
+    keys = {
+      {
+        "<leader>fg",
+        ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+        desc = "Live grep args",
+      },
+    },
+  },
+  {
     "uga-rosa/ccc.nvim",
     config = function()
       local ccc = require "ccc"
